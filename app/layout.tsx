7d6978415aa401e27c77507e0b59913600e7001a
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-17 06:16:54 -0400

    continuing to add modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal/>
+          <Modal actionLabel="Submit" isOpen title="Modal" />
           <Navbar/>
         </ClientOnly>
         {children}
