0160daba63382b53e2ce065952a180ea902b204f
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-19 01:30:28 -0400

    added the ability to register and signInwith Google and Github

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -19,6 +19,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 
 const RegisterModal = () => {
@@ -99,13 +100,13 @@ const RegisterModal = () => {
             outline 
             label="Continue with Google"
             icon={FcGoogle}
-            onClick={() => {}} 
+            onClick={() => signIn('google')} 
           />
           <Button
             outline 
             label="Continue with Github"
             icon={AiFillGithub}
-            onClick={() => {}}
+            onClick={() => signIn('github')}
           />
           <div 
             className="
