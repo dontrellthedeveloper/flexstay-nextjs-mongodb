6175b8e1ba1311dd3fa2008c3058a85d01dbb629
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-20 17:41:35 -0400

    added full search functionality and the ability to filter for properties using categories and reservations

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
 
 
 
@@ -37,6 +38,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <SearchModal/>
           <RentModal/>
           <LoginModal/>
           <RegisterModal/>
