582b33f51290df7cd5a350e1136e66410c95c55a
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-20 04:29:35 -0400

    added the ability to fetch listings from the database to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,54 @@
-import Image from 'next/image'
+import Container from "@/app/components/navbar/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
-export default function Home() {
+import getListings  from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
+
+// interface HomeProps {
+//   searchParams: IListingsParams
+// };
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'></div>
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
+
+export default Home;
