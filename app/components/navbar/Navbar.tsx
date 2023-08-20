8e1d179ddc2c64a49c1c516e403d4130b8e64a21
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-19 20:13:09 -0400

    added categories with query params

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -25,6 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({
           </div>
         </Container>
       </div>
+      <Categories/>
     </div>
   );
 };
